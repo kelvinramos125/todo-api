800ed53028ed4a13360bac1d8be7515f691c07a3
R2H <R2H@adminisatorsmbp.attlocal.net>
2020-07-27 16:06:40 -0400

    Fully done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
-const { values } = require('mysql2/lib/constants/charset_encodings');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -12,52 +11,54 @@ const connection = mysql.createConnection({
 
 try {
     connection.connect();
-}catch (e) {
-    console.log('Oops. Connection to MySQL failed');
+} catch (e) {
+    console.log('connection failed');
     console.log(e);
 }
 
-console.log(connection);
-
 const api = express();
-api.use(express.static(__dirname + '/public'))
-api.use(bodyParser.json());
+api.use(express.static(__dirname + '/public'));
+api.use(bodyParser.json())
 
 api.listen(3000, () => {
-    console.log('API up and running');
+    console.log('API up and running!');
 });
 
-api.get('/task', (req,res) => {
-    connection.query('SELECT * FROM task ORDER BY created DESC', (error, result) => {
-        if (error) return res.json({ error: error});
+api.get('/task', (req, res) => {
+    connection.query('SELECT * FROM task ORDER BY created DESC', (error, results) => {
+        if (error) return res.json({ error: error });
 
-        res.json({
-            todo: results.filter(item => !item.completed),
-            completed: results.filter(item => item.completed)
+        res.json(results);
+    });
+});
+
+api.post('/task/add', (req, res) => {
+    connection.query('INSERT INTO task (description) VALUES (?)', [req.body.item], (error, results) => {
+        if (error) return res.json({ error: error });
+
+        connection.query('SELECT LAST_INSERT_ID() FROM task', (error, results) => {
+            if (error) return res.json({ error: error });
+
+            res.json({
+                id: results[0]['LAST_INSERT_ID()'],
+                description: req.body.item
+            });
         });
     });
 });
 
-api.post('/task/:id/remove', (req, res) => {
-    connection.query('DELETE FROM task WHERE id = ?', [req.params.id], (erro, results) => {
-        if (error) return res.json({error: error});
+api.post('/task/:id/update', (req, res) => {
+    connection.query('UPDATE task SET completed = ? WHERE id = ?', [req.body.completed, req.params.id], (error, results) => {
+        if (error) return res.json({ error: error });
 
         res.json({});
     })
 })
 
-api.post('/task/add', (req, res) =>{
-    console.log(req.body);
-    connection.query('INSERT INTO task (description) VALUES(?)', [req.body.item], (error, result) =>{
-        if (error) return res.json({ error: error});
+api.post('/task/:id/remove', (req, res) => {
+    connection.query('DELETE FROM task WHERE id = ?', [req.params.id], (error, results) => {
+        if (error) return res.json({ error: error });
 
-        connection.query('SELECT LAST_INSERT_ID() FROM task',(error, result) => {
-            if (error) return res.json({ error: error});
-        
-            res.json({
-                id: results[0]['LAST_INSERTED_ID'],
-                description: req.body.item
-            });
-        })
-    })
+        res.json({});
+    });
 });
